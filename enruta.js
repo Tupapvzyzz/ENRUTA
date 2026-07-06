d46e77b1c55b51a39b247369d51892ca3c04d3e0
Tupapvzyzz <140374611+Tupapvzyzz@users.noreply.github.com>
2026-07-05 20:38:24 -0500

    Update enruta.js

--- enruta.js
+++ enruta.js
@@ -38,3 +38,49 @@ function abrirRepartidorListo() {
   ocultar();
   document.getElementById("repartidorListo").classList.add("active");
 }
+let pedidos = JSON.parse(localStorage.getItem("pedidosEnruta")) || [];
+
+function abrirHomeCliente() {
+  ocultar();
+  document.getElementById("homeCliente").classList.add("active");
+}
+
+function calcularPrecio() {
+  const km = parseFloat(document.getElementById("kmPedido").value) || 0;
+  const precio = km * 1.10;
+
+  document.getElementById("precioPedido").textContent =
+    "S/ " + precio.toFixed(2);
+}
+
+function publicarPedido() {
+  const origen = document.getElementById("origenPedido").value.trim();
+  const destino = document.getElementById("destinoPedido").value.trim();
+  const tipo = document.getElementById("tipoPaquete").value.trim();
+  const km = parseFloat(document.getElementById("kmPedido").value) || 0;
+  const precio = km * 1.10;
+
+  if (!origen || !destino || !tipo || km <= 0) {
+    alert("Completa todos los datos del pedido.");
+    return;
+  }
+
+  const pedido = {
+    id: Date.now(),
+    origen,
+    destino,
+    tipo,
+    km,
+    precio: precio.toFixed(2),
+    comision: (precio * 0.09).toFixed(2),
+    repartidorRecibe: (precio * 0.91).toFixed(2),
+    estado: "publicado"
+  };
+
+  pedidos.unshift(pedido);
+  localStorage.setItem("pedidosEnruta", JSON.stringify(pedidos));
+
+  document.getElementById("pedidoPublicado").classList.remove("hidden");
+
+  alert("Pedido publicado correctamente.");
+}
